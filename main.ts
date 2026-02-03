d79bd29c40ff0d150b999bb5f855e3053675a086
arjunherat <arjunherat12@hotmail.com>
2026-02-03 11:20:09 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
+let strip: neopixel.Strip = null
 let ir_waarde = 0
 IR.IR_init()
 let vooruit = 253
 let rechts = 6
 let achteruit = 125
 let links = 31
-let stop = 0
+let stop = 240
 basic.forever(function () {
     ir_waarde = IR.IR_read()
+    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
 })
 basic.forever(function () {
     if (ir_waarde == vooruit) {
@@ -17,6 +20,7 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
     }
     if (ir_waarde == achteruit) {
         basic.showLeds(`
@@ -26,6 +30,7 @@ basic.forever(function () {
             . # # # .
             . . # . .
             `)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CCW, 100)
     }
     if (ir_waarde == links) {
         basic.showLeds(`
@@ -35,6 +40,8 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 45)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 45)
     }
     if (ir_waarde == rechts) {
         basic.showLeds(`
@@ -44,6 +51,8 @@ basic.forever(function () {
             . . . # .
             . . # . .
             `)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 45)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 45)
     }
     if (ir_waarde == stop) {
         basic.showLeds(`
@@ -53,5 +62,6 @@ basic.forever(function () {
             . # # # .
             . . . . .
             `)
+        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
     }
 })
